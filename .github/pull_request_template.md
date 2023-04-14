2f6b351c91427fcf1b8b36559fbd8849c9a733a6
Marcel Pintó Biescas <marcelpinto@google.com>
2023-04-14 15:04:58 +0200

    Setup GitHub configuration
    - Fix Actions
    - Use issues templates
    - Fix spotless and remove unused stuff
    
    Change-Id: Ia838734d60e09efd040d2780a7387d09a79a673f

--- .github/pull_request_template.md
+++
@@ -1,5 +0,0 @@
-# Description #
-
-Path: `[add path to the area or affected sample]`
-
-Please add some info about the changes. 
--- gradle/spotless-init.gradle.kts
+++ gradle/spotless-init.gradle.kts
@@ -41,13 +41,21 @@ rootProject {
     extensions.configure<com.diffplug.gradle.spotless.SpotlessExtensionPredeclare> {
         kotlin {
             target("**/*.kt")
-            ktlint(ktlintVersion)
-            licenseHeaderFile(rootProject.file("spotless/copyright.txt"))
+            targetExclude("**/build/**/*.kt")
+            ktlint(ktlintVersion).userData(mapOf("android" to "true"))
+            licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
         }
-        groovyGradle {
-            target("**/*.gradle")
-            greclipse().configFile(rootProject.file("spotless/greclipse.properties"))
-            licenseHeaderFile(rootProject.file("spotless/copyright.txt"), "(buildscript|apply|import|plugins)")
+        format("kts") {
+            target("**/*.kts")
+            targetExclude("**/build/**/*.kts")
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        }
+        format("xml") {
+            target("**/*.xml")
+            targetExclude("**/build/**/*.xml")
+            // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
+            licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
         }
     }
 }
