f72af4ef332829e6c97d1e6bd14ca5b8309c4003
Marcel Pintó Biescas <marcelpinto@google.com>
2023-04-17 15:05:05 +0200

    Add test to run all samples
    - Open the MainActivity
    - For each sample > search it > Open it and go back
    
    Change-Id: I4275137a7d7116241ca0bdd55662455c09f7ff06

---
+++ app/src/androidTest/java/com/example/platform/app/NavigationTest.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.platform.app
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.filterToOne
+import androidx.compose.ui.test.hasImeAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.text.input.ImeAction
+import androidx.test.espresso.Espresso
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Tests all the samples are present and open.
+ *
+ * Note: consider changing the test to use the TestNavController to control navigation instead
+ */
+@HiltAndroidTest
+class NavigationTest {
+
+    /**
+     * Manages the components' state and is used to perform injection on your test
+     */
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    /**
+     * Use the primary activity to initialize the app normally.
+     */
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Test
+    fun testSamplesOpen() {
+        val searchLabel = "Search button"
+        composeTestRule.apply {
+            onNodeWithText(activity.getString(R.string.app_name)).assertIsDisplayed()
+            onNodeWithContentDescription(searchLabel).performClick()
+            activity.catalogSamples.forEach {
+                onNode(hasImeAction(ImeAction.Search)).performTextInput(it.name)
+                onAllNodesWithText(it.name)
+                    .filterToOne(hasImeAction(ImeAction.Search).not() and hasText(it.description))
+                    .assertIsDisplayed()
+                    .performClick()
+                Espresso.pressBack()
+                onNodeWithContentDescription(searchLabel).performClick()
+            }
+        }
+    }
+}
