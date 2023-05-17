95fc8d442db9e2b042dfe7af39f844799039ae2f
Yuichi Araki <yaraki@google.com>
2023-05-17 17:58:50 +0900

    ConstraintLayout: Update readme
    Change-Id: Ib50d5737c491fc079b7d6c7435e905e6dd55a907

--- samples/user-interface/constraintlayout/src/main/java/com/example/platform/ui/constraintlayout/MotionLayout.kt
+++ samples/user-interface/constraintlayout/src/main/java/com/example/platform/ui/constraintlayout/MotionLayout.kt
@@ -76,21 +76,21 @@ class ImageFilter1Fragment : Fragment(R.layout.motion_04_imagefilter)
 class ImageFilter2Fragment : Fragment(R.layout.motion_05_imagefilter)
 
 @Sample(
-    name = "MotionLayout - 06. Keyframe",
+    name = "MotionLayout - 06. Keyframe position",
     description = "Use a simple keyframe to change the interpolated motion",
     documentation = "https://developer.android.com/develop/ui/views/animations/motionlayout",
 )
 class KeyframePositionFragment : Fragment(R.layout.motion_06_keyframe)
 
 @Sample(
-    name = "MotionLayout - 07. Keyframe",
+    name = "MotionLayout - 07. Keyframe interpolation",
     description = "More complex keyframe, adding rotation interpolation",
     documentation = "https://developer.android.com/develop/ui/views/animations/motionlayout",
 )
 class KeyframeInterpolationFragment : Fragment(R.layout.motion_07_keyframe)
 
 @Sample(
-    name = "MotionLayout - 08. Keyframe",
+    name = "MotionLayout - 08. Keyframe cycle",
     description = "Basic example of using a keyframe cycle",
     documentation = "https://developer.android.com/develop/ui/views/animations/motionlayout",
 )
