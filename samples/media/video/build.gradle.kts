8e85c28fd77774043b2c710f93ffaa0e4c27ae0e
Caren Chang <carenc@google.com>
2024-11-14 20:08:52 -0800

    specifically compile video samples with api 35

--- samples/media/video/build.gradle.kts
+++ samples/media/video/build.gradle.kts
@@ -22,6 +22,7 @@ plugins {
 android {
     namespace = "com.example.platform.media.video"
     viewBinding.isEnabled = true
+    compileSdk = 35
 
     androidResources {
         noCompress += "tflite"
