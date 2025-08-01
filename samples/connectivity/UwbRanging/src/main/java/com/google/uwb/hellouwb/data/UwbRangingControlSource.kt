30f11f583b640fd676162cad09b6178bb7b7a8b1
ziyiw <ziyiw@google.com>
2025-08-01 18:41:35 +0000

    Add HelloUwb ranging demoapp.
    Change-Id: Ib49abaaaf50a5bc30f27b90bd4302b8eb3ee65f3

---
+++ samples/connectivity/UwbRanging/src/main/java/com/google/uwb/hellouwb/data/UwbRangingControlSource.kt
@@ -0,0 +1,43 @@
+/*
+ *
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.google.uwb.hellouwb.data
+
+import com.google.uwb.uwbranging.EndpointEvents
+import com.google.uwb.uwbranging.UwbEndpoint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+interface UwbRangingControlSource {
+
+    fun observeRangingResults(): Flow<EndpointEvents>
+
+    var deviceType: DeviceType
+
+    var configType: ConfigType
+
+    fun updateEndpointId(id: String)
+
+    fun start()
+
+    fun stop()
+
+    fun sendOobMessage(endpoint: UwbEndpoint, message: ByteArray)
+
+    val isRunning: StateFlow<Boolean>
+}
