921e728e7bec0d9cfdcb04523543e9a5b9d8d2dd
Aaron Labiaga <alabiaga@google.com>
2026-01-29 22:04:05 -0700

    Add migration of PiP Stopwatch example and fix manifest merge issue
    Change-Id: If92a33c5b9fd1c1a1f5ed6de1aaddcf47a642883

--- samples/user-interface/picture-in-picture/build.gradle.kts
+++ samples/user-interface/picture-in-picture/build.gradle.kts
@@ -27,7 +27,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
