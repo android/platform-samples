7ed7db4d7926ada9b4a97317325e74085a101f80
Yacine Rezgui <yrezgui@google.com>
2026-07-09 16:38:03 +0200

    refactor: prune unused dependencies and plugins from version catalog and configure IDE project settings

--- samples/graphics/pdf/build.gradle.kts
+++ samples/graphics/pdf/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
-//    implementation(libs.androidx.ui.tooling.preview)
+
     implementation(libs.androidx.material3)
 }
--- samples/user-interface/constraintlayout/build.gradle.kts
+++ samples/user-interface/constraintlayout/build.gradle.kts
@@ -16,8 +16,8 @@
 
 plugins {
     alias(libs.plugins.android.library)
-//    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.android)}
+    alias(libs.plugins.kotlin.android)
+}
 
 android {
     namespace = "com.example.platform.ui.constraintlayout"
